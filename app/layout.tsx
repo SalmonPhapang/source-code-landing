73e123ab7208f2c48643e8f0ab0676255abcd76d
Salmon Phapang <phapang.salmon@gmail.com>
2026-03-27 12:27:36 +0200

    added schema markup

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,8 +24,69 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const schemaMarkup = {
+    "@context": "https://schema.org",
+    "@graph": [
+      {
+        "@type": "Organization",
+        "@id": "https://www.sourcecodes.co.za/#organization",
+        "name": "Source Code Development",
+        "url": "https://www.sourcecodes.co.za/",
+        "logo": "https://www.sourcecodes.co.za/logo.png",
+        "contactPoint": {
+          "@type": "ContactPoint",
+          "telephone": "+27-72-541-3028",
+          "contactType": "customer service",
+          "email": "info@sourcecodes.co.za",
+          "areaServed": "ZA",
+          "availableLanguage": "English"
+        },
+        "address": {
+          "@type": "PostalAddress",
+          "streetAddress": "11740 sekwati street, zone 7a",
+          "addressLocality": "Sebokeng",
+          "postalCode": "1983",
+          "addressCountry": "ZA"
+        }
+      },
+      {
+        "@type": "WebSite",
+        "@id": "https://www.sourcecodes.co.za/#website",
+        "url": "https://www.sourcecodes.co.za/",
+        "name": "Source Code Development",
+        "description": "Custom software development, web design, and mobile application development.",
+        "publisher": {
+          "@id": "https://www.sourcecodes.co.za/#organization"
+        }
+      },
+      {
+        "@type": "ProfessionalService",
+        "@id": "https://www.sourcecodes.co.za/#service",
+        "name": "Source Code Development",
+        "image": "https://www.sourcecodes.co.za/logo.png",
+        "url": "https://www.sourcecodes.co.za/",
+        "telephone": "+27-72-541-3028",
+        "address": {
+          "@type": "PostalAddress",
+          "streetAddress": "11740 sekwati street, zone 7a",
+          "addressLocality": "Sebokeng",
+          "postalCode": "1983",
+          "addressCountry": "ZA"
+        },
+        "priceRange": "ZAR",
+        "openingHours": "Mo-Fr 08:00-17:00"
+      }
+    ]
+  };
+
   return (
     <html lang="en" className={`${inter.variable} ${jetbrainsMono.variable}`}>
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaMarkup) }}
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
